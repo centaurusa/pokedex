4d26ae2d275467e34e4ee997402b70864ad5e983
Ilya Shumsky <Thunderbolt@Ilyas-MacBook-Pro.local>
2015-11-13 21:27:26 +0200

    first project commit

---
+++ pokedex-by-shumsky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-shumsky
+//
+//  Created by Ilya Shumsky on 13/11/2015.
+//  Copyright © 2015 Ilya Shumsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
